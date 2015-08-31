35e460a47a1fecbac7139e77fdd347f87fb52238
Markus Inger <git@markus-inger.de>
2015-08-31 23:19:03 +0200

    translate sidebar title

--- src/main/controller.js
+++ src/main/controller.js
@@ -2,9 +2,12 @@ module.exports.name = 'app';
 
 module.exports.controller = /* @ngInject */ function ($translate) {
 
+    var self = this;
     this.miniLogoUrl = require('../assets/image/mini-logo.png');
 
-    this.title = 'title';
+    $translate(['APP.SIDEBAR_TITLE']).then(function (translations) {
+        self.title = translations['APP.SIDEBAR_TITLE'];
+    });
 
     this.changeLanguage = function (langKey) {
         $translate.use(langKey);
