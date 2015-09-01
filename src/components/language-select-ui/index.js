5ece1e33a46c68fe8eb50c9b02039ecbdedc16f8
Markus Dethlefsen <dethlefsen@cleverbridge.com>
2015-09-01 15:16:47 +0200

    add dependencies to language select

--- src/components/language-select-ui/index.js
+++ src/components/language-select-ui/index.js
@@ -3,7 +3,8 @@ require('./index.scss');
 
 module.exports = angular
     .module('component.ui.languageSelect', [
-
+        require('angular-material'),
+        require('angular-translate')
     ])
     .directive(require('./module').name, require('./module').component)
     .name;
