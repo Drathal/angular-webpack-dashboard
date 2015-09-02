83da0614d582d6490c66d7ccda1421059ae1ffc1
Markus Inger <git@markus-inger.de>
2015-09-02 23:07:37 +0200

    prepare end to end tests

---
+++ globalStepDefinitions.js
@@ -0,0 +1,8 @@
+module.exports = function() {
+
+    this.Given(/^the page has the "userinfo-ui" directive on it$/, function (callback) {
+        // Write code here that turns the phrase above into concrete actions
+        callback.pending();
+    });
+
+};
