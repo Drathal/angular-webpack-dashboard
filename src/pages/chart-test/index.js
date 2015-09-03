e59116d2f1096c3094b9b014073cea8974fde5b5
Markus Inger <git@markus-inger.de>
2015-09-04 01:54:35 +0200

    add charts (no component pattern yet)

--- src/pages/chart-test/index.js
+++ src/pages/chart-test/index.js
@@ -1,11 +1,12 @@
 var angular = require('angular');
-require('chart.js/Chart.js');
+require('imports?this=>window!chart.js');
+require('angular-chart.js');
+require('../../../node_modules/angular-chart.js/dist/angular-chart.css');
 
 module.exports = angular
     .module('app.charttest', [
-        require('../../components/menu-service')
-
-        //,require('angular-chart.js')
+        require('../../components/menu-service'),
+        'chart.js'
     ])
     .config(require('./chart-test.route.js').config)
     .run(require('./chart-test.route.js').run)
