8dbec41734583dfad98e54de232a95cc07ad6932
Markus Dethlefsen <dethlefsen@cleverbridge.com>
2015-09-07 11:49:22 +0200

    ignore bundles in coverage

--- webpack/webpack.test.js
+++ webpack/webpack.test.js
@@ -11,7 +11,7 @@ module.exports = function () {
             preLoaders: [
                 {
                     test: /\.js$/,
-                    exclude: [/tepDefinitions\.js/, /node_modules/, /\.spec\.js$/],
+                    exclude: [/tepDefinitions\.js/, /node_modules/, /\.spec\.js$/, /bundle\.js/],
                     loader: 'istanbul-instrumenter-loader'
                 }
             ],
