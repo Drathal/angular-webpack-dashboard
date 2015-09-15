01bb213e238fb1122065ee0875420db082d9277d
Markus Dethlefsen <dethlefsen@cleverbridge.com>
2015-09-15 16:19:29 +0200

    add simple test for bind-html-compile refactor bind-html-compile (we don't need a  sub scope here)

--- src/components/bind-html-compile/bind-html-compile.ctrl.js
+++ src/components/bind-html-compile/bind-html-compile.ctrl.js
@@ -7,12 +7,8 @@ export default /*@ngInject*/  function bindHtmlCompile($compile) {
                     return scope.$eval(attrs['bindHtmlCompile']);
                 },
                 (value) => {
-                    let compileScope = scope;
                     element.html(value && value.toString());
-                    if (attrs['bindHtmlScope']) {
-                        compileScope = scope.$eval(attrs['bindHtmlScope']);
-                    }
-                    $compile(element.contents())(compileScope);
+                    $compile(element.contents())(scope);
                 }
             );
         }
---
+++ src/components/bind-html-compile/bind-html-compile.spec.js
@@ -0,0 +1,27 @@
+describe('component', function () {
+    describe('bind-html-file', function () {
+
+        describe('directive', function () {
+
+            var element;
+            var $scope;
+
+            beforeEach(angular.mock.module(require('./index.js')));
+            beforeEach(inject(function ($rootScope, $compile) {
+                element = angular.element('<span bind-html-compile="teststring"></span>');
+                $scope = $rootScope;
+                $scope.teststring = 'test <b>test <span bind-html-compile="teststring2"></span></b>';
+                $scope.teststring2 = 'test <b class="testclass">test2</b>';
+                $compile(element)($scope);
+                $scope.$digest();
+            }));
+
+            it('should render the app directive', function () {
+                expect($(element).find('b.testclass').length).to.equal(1);
+            });
+
+        });
+
+    });
+
+});
