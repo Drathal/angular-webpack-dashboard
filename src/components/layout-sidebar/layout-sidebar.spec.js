a74fec05b05e4651fa8a888110db33608a6a8251
Markus Inger <git@markus-inger.de>
2015-09-15 19:45:52 +0200

    more layout sidebar tests add sinon to kown globals

--- src/components/layout-sidebar/layout-sidebar.spec.js
+++ src/components/layout-sidebar/layout-sidebar.spec.js
@@ -1,5 +1,57 @@
+var LayoutSidebar = require('./layout-sidebar');
+
 describe('component', function () {
     describe('layout-sidebar', function () {
+
+        describe('controller', function () {
+            var layout;
+            var rootScope;
+            var controller;
+            var translate;
+            var mdSidenav;
+            var toggle;
+            var close;
+
+            beforeEach(angular.mock.module('component.service.translationLoader'));
+            beforeEach(angular.mock.module('pascalprecht.translate'));
+            beforeEach(angular.mock.module('ngMaterial'));
+            beforeEach(inject(function ($rootScope, $translate, $mdSidenav) {
+                rootScope = $rootScope;
+                translate = $translate;
+                toggle = sinon.stub();
+                close = sinon.stub();
+                $mdSidenav = () => {
+                    return {
+                        toggle: toggle,
+                        close: close
+                    };
+                };
+                mdSidenav = $mdSidenav;
+                layout = new LayoutSidebar();
+                controller = new layout.controller($rootScope, $translate, $mdSidenav);
+                $rootScope.$digest();
+            }));
+
+            it('should define mini logo', function () {
+                expect(controller.miniLogoUrl).to.have.string('.png');
+            });
+
+            it('should have a toggleSidebar function', function () {
+                expect(controller.toggleSidebar).to.be.a('function');
+                controller.toggleSidebar();
+                expect(toggle.called).to.equal(true);
+            });
+
+            it('should close the sidenav on $locationChangeSuccess', function () {
+                rootScope.$emit('$locationChangeSuccess');
+                expect(close.called).to.equal(true);
+            });
+
+            it('should set a title', function () {
+                expect(controller.title).to.be.a('string');
+            });
+
+        });
 
         describe('directive', function () {
 
